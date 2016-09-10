7c0fce21cd77672c0455a30221e6f9604d5a71fe
cmtp <christian@hotmail.com>
2016-09-10 00:12:18 -0400

    add example with custom buttons for control the player audio

--- app/player/js/app.js
+++ app/player/js/app.js
@@ -6,5 +6,18 @@ var audio;
 function initPlayer() {
     audio = new Audio();
     audio.src = 'music/music.mp3';
-    audio.play();
+    $('#play').click( function () {
+        playPause();
+    });
+}
+
+function playPause() {
+    if( audio.paused ) {
+        audio.play();
+        $('#pl-pa').addClass('fa-pause').removeClass('fa-play');
+    }
+    else {
+        audio.pause();
+        $('#pl-pa').addClass('fa-play').removeClass('fa-pause');
+    }
 }
